89e4552ab0b660a878f6a31eb3165fac523b6ec2
Francesco Zuccaro <fzuccaro@c2r4p7.42firenze.it>
2025-11-13 14:01:13 +0100

    commit piu figo del progetto

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gpirozzi <gpirozzi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fzuccaro <fzuccaro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 16:42:12 by gpirozzi          #+#    #+#             */
-/*   Updated: 2025/11/12 12:44:28 by gpirozzi         ###   ########.fr       */
+/*   Updated: 2025/11/12 17:48:51 by fzuccaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,13 @@
 
 int main(int argc, char **argv)
 {
+    signal(SIGINT, handle_sigint);
+    
     if (argc != 3)
     {
         std::cerr << "Invalid input" << std::endl;
         return (1);
     }
-
     char    *endptr;
     int portNbr = std::strtol(argv[1], &endptr,10);
     if (errno == ERANGE || *endptr != '\0' || (portNbr < 6660 && portNbr > 6699))
