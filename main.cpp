83338020e4b760d8321c2e99f578eddbb99c8f27
Giovanni Pirozzi <gpirozzi@c2r4p6.42firenze.it>
2025-11-14 15:53:48 +0100

    PASS USER NCK implementati

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: redei-ma <redei-ma@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gpirozzi <gpirozzi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 16:42:12 by gpirozzi          #+#    #+#             */
-/*   Updated: 2025/11/14 11:08:16 by redei-ma         ###   ########.fr       */
+/*   Updated: 2025/11/14 15:36:47 by gpirozzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,11 @@ int main(int argc, char **argv)
     std::string input2(argv[2]);
 	try
 	{
+        for (size_t i = 0; i < input2.size(); i++)
+        {
+            if (std::isspace(input2[2]) || std::isprint(input2[2]))
+                throw std::invalid_argument("Invalid characher in password");
+        }
 		Server  myServer(portNbr, input2);
 		myServer.run();
 	}
