882e1fa0415b64a446ce86bcb0c5d9b7fb1085fc
Giovanni Pirozzi <gpirozzi@c2r4p5.42firenze.it>
2025-11-20 18:33:03 +0100

    fix NICK e BOT

--- srcs/commands/bot.cpp
+++ srcs/commands/bot.cpp
@@ -5,9 +5,15 @@
 #include "ReplyHandler.hpp"
 #include <iostream>
 
-const std::string _BOTprefix = ":irc.rfg.com BOT:\n";
-
 #define REGISTERED_BOT_CMD(enum, name) botMapExecuter[enum] = name##Command
+
+const std::string _SERVERNAME = "irc.rfg.com";
+
+static void sendBotLine(User* executer, const std::string& text)
+{
+	std::string msg = ":" + _SERVERNAME + " NOTICE " + executer->getNickName() + " :" + text;
+	executer->sendMessage(msg);
+}
 
 typedef enum s_bot
 {
@@ -33,67 +39,57 @@ t_bot	findCommandToExec(const std::string& input)
 	return (ERROR);
 }
 
-static void	sendBotCommands(User* executer)
+static void		sendBotCommands(User* executer)
 {
-	std::string msg = ":" + executer->getNickName() + "!" + executer->getUserName() + "@" + executer->getHostNameAsString() +
-	" Use the command BOT with arg:\n" +
-	"/help\n" + 
-	"/users\n" +
-	"/channels";
-	executer->sendMessage(msg);
+	sendBotLine(executer, "Use the command BOT with arg:");
+	sendBotLine(executer, "/help");
+	sendBotLine(executer, "/users");
+	sendBotLine(executer, "/channels");
+	sendBotLine(executer, "/game");
 }
 
 void	helpCommand(User* executer, const Server& server)
 {
 	(void)server;
 
-	std::string msg = _BOTprefix +
-	"NICK with something for change your nickname\n" +
-	"INVITE with nickName and #nameChannel for invite a user to a channel\n" +
-	"JOIN with #nameChannel for join/create a channel\n" +
-	"KICK with nickName and #nameChannel for kick a user from a channel\n" +
-	"MODE with #nameChannel and modes(+/- i t k o l) for set modes to a channel\n" +
-	"TOPIC with #nameChannel and topic for set a topic to a channel\n"
-	"PRIVMSG with nickName/#nameChannel and message for send a message to a user/channel\n" +
-	"PART with #nameChannel for leave a channel\n" +
-	"QUIT for disconnect from server\n" +
-	"BOT with arg: /help | /users | /channels\n";
-	executer->sendMessage(msg);
+	sendBotLine(executer, "NICK with something for change your nickname");
+	sendBotLine(executer, "INVITE with nickName and #nameChannel for invite a user to a channel");
+	sendBotLine(executer, "JOIN with #nameChannel for join/create a channel");
+	sendBotLine(executer, "KICK with nickName and #nameChannel for kick a user from a channel");
+	sendBotLine(executer, "MODE with #nameChannel and modes(+/- i t k o l) for set modes to a channel");
+	sendBotLine(executer, "TOPIC with #nameChannel and topic for set a topic to a channel");
+	sendBotLine(executer, "PRIVMSG with nickName/#nameChannel and message for send a message to a user/channel");
+	sendBotLine(executer, "PART with #nameChannel for leave a channel");
+	sendBotLine(executer, "QUIT for disconnect from server");
+	sendBotLine(executer, "BOT with arg: /help | /users | /channels");
 }
 
 void	usersCommand(User* executer, const Server& _server)
 {
 	std::vector<User*>	userVector = _server.getUserVector();
-	std::string	msg = _BOTprefix;
-
+	if (userVector.empty())
+	{
+		sendBotLine(executer, "No users connected");
+		return;
+	}
 	for (size_t i = 0; i < userVector.size(); i++)
 	{
-		msg += userVector[i]->getNickName();
-		if (i < userVector.size() - 1)
-			msg += "\n";
+		sendBotLine(executer, userVector[i]->getNickName());
 	}
-	executer->sendMessage(msg);
 }
 
 void	channelsCommand(User* executer, const Server& _server)
 {
 	std::vector<Channel*>	channelVector = _server.getChannelVector();
-	std::string	msg = _BOTprefix;
-
 	if (channelVector.empty())
 	{
-		msg += "No channels created";
-		executer->sendMessage(msg);
-		return ;
+		sendBotLine(executer, "No channels currently");
+		return;
 	}
-
 	for (size_t i = 0; i < channelVector.size(); i++)
 	{
-		msg += channelVector[i]->getName();
-		if (i < channelVector.size() - 1)
-			msg += "\n";
+		sendBotLine(executer, channelVector[i]->getName());
 	}
-	executer->sendMessage(msg);
 }
 
 static void	setBotMapExecuter(std::map<t_bot, void(*)(User*, const Server&)>& botMapExecuter)
@@ -120,9 +116,7 @@ void	CommandHandler::botCommand(User* executer, std::vector<std::string>& comman
 	t_bot	command = findCommandToExec(commandArgs[1]);
 	if (command == ERROR)
 	{
-		std::string msg = _BOTprefix + executer->getNickName() + "!" +
-		executer->getUserName() + "@" + executer->getHostNameAsString() + " BOT " + " :" + "Invalid command";
-		executer->sendMessage(msg);
+		sendBotLine(executer, "Invalid command");
 		return ;
 	}
 
