ee1368bc962afad6080eb237739dca132cc2091d
Renato <renato.deimarini@gmail.com>
2025-11-20 14:21:29 +0100

    fix kick

--- srcs/commands/kick.cpp
+++ srcs/commands/kick.cpp
@@ -12,9 +12,65 @@
 **	KICK #ch1,#ch2 user1,user2 :motivo			// Utente e canali abbinati
 */
 
-void kicking()
+// Helper per validare il canale e i permessi dell'operatore
+static Channel* validateChannelAndOperator(User* executer, const std::string& channelName, Server& server)
 {
-	//to do
+    Channel* channel = server.getChannelByName(channelName);
+    if (!channel)
+    {
+        ReplyHandler::errorHandler(ERR_NOSUCHCHANNEL, *executer, channelName, "KICK");
+        return NULL;
+    }
+
+    if (!channel->isMember(executer))
+    {
+        ReplyHandler::errorHandler(ERR_NOTONCHANNEL, *executer, channelName, "KICK");
+        return NULL;
+    }
+
+    if (!channel->isOperator(executer))
+    {
+        ReplyHandler::errorHandler(ERR_CHANOPRIVSNEEDED, *executer, channelName, "KICK");
+        return NULL;
+    }
+
+    return channel;
+}
+
+static User* validateTargetUser(User* executer, const std::string& userName, Channel* channel, Server& server)
+{
+    User* targetUser = server.getUserByNick(userName);
+    if (!targetUser)
+    {
+        ReplyHandler::errorHandler(ERR_NOSUCHNICK, *executer, userName, "KICK");
+        return NULL;
+    }
+    
+    if (targetUser == executer)
+    {
+        std::string message = ":" + executer->getNickName() + "!" + executer->getUserName() + 
+                             "@irc.rfg.com KICK " + channel->getName() + " " + 
+                             targetUser->getNickName() + " :You can't kick yourself";
+        executer->sendMessage(message);
+        return NULL;
+    }
+
+    if (!channel->isMember(targetUser))
+    {
+        ReplyHandler::errorHandler(ERR_USERNOTINCHANNEL, *executer, userName, "KICK");
+        return NULL;
+    }
+    
+    return targetUser;
+}
+
+static void kicking(User* executer, Channel* channel, User* targetUser, const std::string& reason)
+{
+	std::string message = ":" + executer->getNickName() + "!" + executer->getUserName() + "@irc.rfg.com" + " KICK " + channel->getName() + " " + targetUser->getNickName() + reason;
+	channel->broadcastMessage(message, NULL);
+	channel->removeUser(targetUser);
+
+	std::cout << "KICK command executed: " << executer->getNickName() << " kicked " << targetUser->getNickName() << " from " << channel->getName() << std::endl;
 }
 
 void	CommandHandler::kickCommand(User* executer, std::vector<std::string>& commandArgs)
@@ -26,49 +82,58 @@ void	CommandHandler::kickCommand(User* executer, std::vector<std::string>& comma
 
 	std::vector<std::string> channelNames = split(commandArgs[0], ',');
 	std::vector<std::string> usersName = split(commandArgs[1], ',');
+	std::string reason = " :";
+	if (commandArgs.size() == 3)
+		reason += commandArgs[2];
+	else
+		reason += executer->getNickName();
 
 	if (channelNames.size() == usersName.size())
 	{
-		for (size_t i = 0; i > channelNames.size(); i++)
+		for (size_t i = 0; i < channelNames.size(); i++)
 		{
-			Channel *channel = _server.getChannelByName(channelNames[i]);
+			Channel *channel = validateChannelAndOperator(executer, channelNames[i], _server);
 			if (!channel)
-				return (ReplyHandler::errorHandler(ERR_NOSUCHCHANNEL, *executer, channelNames[i], "KICK"));
-			
-			if (!channel->isMember(executer))
-				return (ReplyHandler::errorHandler(ERR_NOTONCHANNEL, *executer, channelNames[i], "KICK"));
-			
-			if (!channel->isOperator(executer))
-				return (ReplyHandler::errorHandler(ERR_CHANOPRIVSNEEDED, *executer, channelNames[i], "KICK"));
+				continue ;
 
-			User* targetUser = _server.getUserByNick(usersName[i]);
+			User* targetUser = validateTargetUser(executer, usersName[i], channel, _server);
 			if (!targetUser)
-				return (ReplyHandler::errorHandler(ERR_NOSUCHNICK, *executer, commandArgs[1], "KICK"));
+				continue ;
 
-			if (targetUser == executer)
-			{
-				std::string message = ":" + executer->getNickName() + "!" + executer->getUserName() + "@irc.rfg.com" + " KICK " + channel->getName() + " " + targetUser->getNickName() + " :You can't kick yourself";;
-				executer->sendMessage(message);
-				return ;
-			}
-			if (!channel->isMember(targetUser))
-				return (ReplyHandler::errorHandler(ERR_USERNOTINCHANNEL, *executer, commandArgs[1], "KICK"));
-			
 			kicking(executer, channel, targetUser, reason);
 		}
 	}
 	else if (channelNames.size() == 1)
 	{
+		Channel *channel = validateChannelAndOperator(executer, channelNames[0], _server);
+		if (!channel)
+			return ;
 
+		for (size_t i = 0; i < usersName.size(); i++)
+		{
+			User* targetUser = validateTargetUser(executer, usersName[i], channel, _server);
+			if (!targetUser)
+				continue;
+
+			kicking(executer, channel, targetUser, reason);
+		}
 	}
 	else if (usersName.size() == 1)
 	{
+		for (size_t i = 0; i < channelNames.size(); i++)
+		{
+			Channel *channel = validateChannelAndOperator(executer, channelNames[i], _server);
+			if (!channel)
+				continue ;
+			User* targetUser = validateTargetUser(executer, usersName[0], channel, _server); // Lo fa N volte anche l'utente e lo stesso ma devo controlalre se e nel canale
+			if (!targetUser)
+				continue ;
 
+			kicking(executer, channel, targetUser, reason);
+		}
 	}
 	else
-	{
-		return ERROR;
-	}
+		return (ReplyHandler::errorHandler(ERR_NEEDMOREPARAMS, *executer, "", "KICK"));
 }
 
 
